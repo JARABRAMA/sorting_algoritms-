4fa83ae42dcba4a7820c1ec84213d77ec58b99fa
JARABRAMA <brayanjaraba2006@gmail.com>
2024-04-02 16:10:02 -0500

    fix type issue

--- test_sorters.py
+++ test_sorters.py
@@ -5,7 +5,7 @@ from sorters import Sorters
 
 class Test(TestCase):
     x: list[int] = [3, 9, 8, 2, 1, 6, 4, 5, 7]
-    orderedList: int = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    orderedList: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     def test_selection_sort(self):
         print(Sorters.selection_sort(self.x))
